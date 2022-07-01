b0187cc96a930b09e7a87aa9eeebfa60846f4856
Aleff Junio Silva Lins Lira <dev.alefflira@gmail.com>
2022-07-01 17:45:48 -0300

    Inclusão do arquivo sobre operador ternário, comentários adicionados aos códigos

--- M2/1_condicional_if.php
+++ M2/1_condicional_if.php
@@ -1,5 +1,12 @@
 <?php
+// Condicional If/Else
+
 $idade = 20;
+
+/*
+Se a idade for menor de 18 anos, então: Menor de idade
+Senão, Maior de idade.
+*/
 
 if($idade < 18) {
   echo 'Menor de idade';
---
+++ M2/2_operador_ternario.php
@@ -0,0 +1,14 @@
+<?php
+// Operador ou Condicional Ternário
+
+$idade = 20;
+
+echo ($idade < 18) ? 'Menor de idade' : 'Maior de idade';
+
+// Armazenando a condição numa variável
+
+$resultado = ($idade < 18) ? 'Menor de idade' : 'Maior de idade';
+
+// Armazenando um Boolean - true ou false
+
+$menorDeIdade = ($idade < 18) ? true : false;
