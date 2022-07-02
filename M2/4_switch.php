f161425a0218988ed703d1c1070f50458af81760
Aleff Junio Silva Lins Lira <dev.alefflira@gmail.com>
2022-07-01 22:02:39 -0300

    Adição dos arquivos relacionados a Switch e While

---
+++ M2/4_switch.php
@@ -0,0 +1,14 @@
+<?php
+// Condicional Switch
+$tipo = 'foto';
+
+switch($tipo) {
+  case 'foto':
+    echo 'Exibindo foto';
+    break;
+  case 'video':
+    echo 'Exibindo video';
+    break;
+  case 'texto':
+    echo 'Exibindo texto';
+}
---
+++ M2/5_loop_while.php
@@ -0,0 +1,9 @@
+<?php
+
+// Loop com While - Enquanto
+$numero = 0;
+
+while($numero < 10) {
+  echo 'N: '.$numero.'<br/>';
+  $numero++;
+}
